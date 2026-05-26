999f2beab16724048f8ea266118346136055a57d
Akhil Naidu <akhilnaidu19@gmail.com>
2026-05-26 14:32:55 +0530

    Fixed parking stauts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,17 +1,31 @@
 import { initializeApp } from "firebase/app";
+
 import { getFirestore } from "firebase/firestore";
+
 import { getStorage } from "firebase/storage";
 
+import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
+
   apiKey: "AIzaSyD-zcUfPLdlpEZCdHz6QULbgHOlmtOCmK4",
+
   authDomain: "carparking-f65bf.firebaseapp.com",
+
   projectId: "carparking-f65bf",
-  storageBucket: "carparking-f65bf.firebasestorage.app",
+
+  storageBucket: "carparking-f65bf.appspot.com",
+
   messagingSenderId: "124435045479",
+
   appId: "1:124435045479:web:83a0ead77364e64b2988c2",
+
 };
 
 export const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+
 export const storage = getStorage(app);
+
+export const auth = getAuth(app);
